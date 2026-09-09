9b7064ce266f9e68014008f42c9599858729418b
apixeldev <owen.winchell@proton.me>
2026-09-09 09:46:04 -0400

    made SSP not print out text
    I feel this is for pretty obvious reasons, printing out your canary is a
    bad idea.

--- src/generic/sec/ssp.c
+++ src/generic/sec/ssp.c
@@ -1,43 +1,16 @@
-#include <stdio.h>
 #include <sec/random.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <panic.h>
 
 uintptr_t __stack_chk_guard = 0x595e9fbd94fda766;
-static bool hasbeeninit = false;
 
 __attribute__((no_stack_protector)) bool sspsetup(void) {
-    (void)hasbeeninit;
-
-    printf("SSP: entered sspsetup()\n");
-
-    printf("SSP: declaring data\n");
     uint64_t data[8];
 
-    printf("SSP: data allocated at %p\n", (void *)data);
+    if (!csprng_getrand((uint8_t *)data)) return false;
 
-    printf("SSP: calling csprng_getrand()\n");
-    if (!csprng_getrand((uint8_t *)data)) {
-        printf("SSP: csprng_getrand() FAILED\n");
-        return false;
-    }
-
-    printf("SSP: csprng_getrand() returned successfully\n");
-
-    printf("SSP: data[0] = 0x%016llx\n",
-           (unsigned long long)data[0]);
-
-    printf("SSP: setting __stack_chk_guard\n");
     __stack_chk_guard = data[0];
-
-    printf("SSP: __stack_chk_guard = 0x%016llx\n",
-           (unsigned long long)__stack_chk_guard);
-
-    printf("SSP: setting hasbeeninit\n");
-    hasbeeninit = true;
-
-    printf("SSP: sspsetup() returning true\n");
     return true;
 }
 
