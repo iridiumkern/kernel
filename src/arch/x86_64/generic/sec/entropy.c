e66898e89178f2534a6a415b5af2b2fa863c919a
apixeldev <owen.winchell@proton.me>
2026-09-13 15:22:43 -0400

    Fixed bug where github actions failed to build due to syntax incompatability in <cpuid.h>

--- src/arch/x86_64/generic/sec/entropy.c
+++ src/arch/x86_64/generic/sec/entropy.c
@@ -1,7 +1,28 @@
 #include <immintrin.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <cpuid.h>
+
+static inline void cpuid(uint32_t leaf, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) {
+    uint32_t a;
+    uint32_t b;
+    uint32_t c;
+    uint32_t d;
+
+    __asm__ volatile (
+        "cpuid"
+        : "=a"(a),
+          "=b"(b),
+          "=c"(c),
+          "=d"(d)
+        : "a"(leaf)
+        : "cc"
+    );
+
+    *eax = a;
+    *ebx = b;
+    *ecx = c;
+    *edx = d;
+}
 
 // Reads from the TSC
 uint64_t rdtsc(void) {
@@ -41,9 +62,9 @@ bool random_u64(uint64_t *out) {
 
     uint32_t eax, ebx, ecx, edx;
 
-    __cpuid(1, eax, ebx, ecx, edx);
+    cpuid(1, &eax, &ebx, &ecx, &edx);
 
-    if (ecx & bit_RDRND) {
+    if (ecx & (1U << 30)) {
         uint64_t value;
 
         if (rdrand64(&value)) {
