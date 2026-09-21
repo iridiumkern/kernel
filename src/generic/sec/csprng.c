5d220b668472cfd3dc4f0ddec9e06ab158ec51da
ThatOSDeveloper <contact@thatosdeveloper.us>
2026-09-20 20:36:43 -0400

    Removed some clutter from the CSPRNG (printf calls, and a few redundent comments)

--- src/generic/sec/csprng.c
+++ src/generic/sec/csprng.c
@@ -4,26 +4,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdio.h>
-#include <debug.h>
 
 static uint64_t state[8] = {0};
 static bool init = false;
 
 bool csprng_init(void) {
     if (init) return false;
-    // Just in case some attacker messes with BSS for example
-    memset(&state, 0, 64);
-    printf("Cleared state\n");
 
     uint64_t dirtystuff = 0;
-    // Quick and dirty entropy, not secure on its own, though assumed to be part of the secret state
     if (!random_u64(&dirtystuff)) return false;
-    printf("random_u64 called\n");
 
-    // Use SHA512 inputing dirtystuff and outputting into state
     sha512_bytes((void*)&dirtystuff, 8, (void*)&state);
-    printf("SHA512 called called!\n");
 
     memset(&dirtystuff, 0, 8);
     init = true;
@@ -33,7 +24,7 @@ bool csprng_init(void) {
 bool csprng_addentropy(void* data, uint64_t size) {
     if (!init) return false;
     if (!range_is_mapped((uintptr_t)data, size)) return false;
-    // Hash the data
+
     uint64_t hasheddata[8] = {0};
     uint64_t newstate[8] = {0};
     sha512_bytes(data, size, (void*)&hasheddata);
