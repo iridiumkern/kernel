8884341332164111118bbcdbe6decd5de825e4f0
ThatOSDeveloper <contact@thatosdeveloper.us>
2026-09-20 20:42:16 -0400

    removed setjmp and longjmp since they were unused

--- src/generic/sec/ssp.c
+++ src/generic/sec/ssp.c
@@ -8,7 +8,7 @@ uintptr_t __stack_chk_guard = 0x595e9fbd94fda766;
 __attribute__((no_stack_protector)) bool sspsetup(void) {
     uint64_t data[8];
 
-    if (!csprng_getrand((uint8_t *)data)) return false;
+    if (!csprng_getrand((uint8_t *)data)) kpanic("Could not setup SSP state, csprng_getrand returned false.");
 
     __stack_chk_guard = data[0];
     return true;
